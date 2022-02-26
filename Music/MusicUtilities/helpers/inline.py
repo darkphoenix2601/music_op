1823fdff2aaebf8f0eb2d805bdd20dedc37ce9c4
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2022-02-26 18:21:26 +0530

    Update inline.py

--- Music/MusicUtilities/helpers/inline.py
+++ Music/MusicUtilities/helpers/inline.py
@@ -36,10 +36,7 @@ def others_markup(videoid, user_id):
                 InlineKeyboardButton(text="✨ ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{UPDATES_CHANNEL}"),
                 InlineKeyboardButton(text="➕ Group Playlist", callback_data=f'group_playlist {videoid}|{user_id}')
             ],
-            [
-                InlineKeyboardButton(text="Get Audio", callback_data=f'gets audio|{videoid}|{user_id}'),
-                InlineKeyboardButton(text="Get Video", callback_data=f'gets video|{videoid}|{user_id}')
-            ],
+
             [
                 InlineKeyboardButton(text="🔙", callback_data=f'goback {videoid}|{user_id}'),
                 InlineKeyboardButton(text="🗑 Close", callback_data=f'close2')
