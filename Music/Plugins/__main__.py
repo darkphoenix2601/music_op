8e8bfb56a3a8a94513fb1efa289b4131519fa4ab
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2022-02-26 16:33:21 +0530

    Add files via upload

---
+++ Music/Plugins/__main__.py
@@ -0,0 +1,2 @@
+##https://github.com/VENOMxCRAZY9
+## @V3NOM_SUPPORT
---
+++ Music/Plugins/start.py
@@ -0,0 +1,102 @@
+import asyncio
+from time import time
+from datetime import datetime
+from Music import BOT_USERNAME
+from Music.config import UPDATES_CHANNEL, GROUP_SUPPORT
+from Music.MusicUtilities.helpers.filters import command
+from Music.MusicUtilities.helpers.command import commandpro
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+    
+   
+
+@Client.on_message(command("start") & filters.private & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/0b4ed6155e5b9814f0ad7.jpg",
+        caption=f"""A Telegram Music Bot Based Mongodb.
+ Add Me To Ur Chat For and Help and And Support Click On Buttons  ...
+💞  These Features A.I Based 
+Powered By [Professor](https://t.me/Professor_Ashu ...
+""",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "➕ ❰ Add me to your group ❱ ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "Help and commands", url=f"https://t.me/Miss_AkshiV1_Updates/9"
+                    ),
+                    InlineKeyboardButton(
+                        "Repo", url="https://t.me/Miss_AkshiV1_Updates/10"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "Channel", url=f"https://t.me/{UPDATES_CHANNEL}"
+                    ),
+                    InlineKeyboardButton(
+                        "Support", url=f"https://t.me/{GROUP_SUPPORT}"
+                    )
+                ]
+                
+           ]
+        ),
+    )
+    
+    
+@Client.on_message(commandpro(["/start", "/alive"]) & filters.group & ~filters.edited)
+async def start(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/188fe491c0264ff74af8a.jpg",
+        caption=f"""Thanks For Adding Me To Ur Chat, For Any Query U Can Join Our Support Groups 🔥♥️""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Join here", url=f"https://t.me/{GROUP_SUPPORT}")
+                ]
+            ]
+        ),
+    )
+
+
+@Client.on_message(command(["repo", "source"]) & filters.group & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/528ea80d84a521c8a9143.jpg",
+        caption=f""" SOURCE AWAILABLE AT @Professor_ashu """,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Repo", url=f"https://t.me/Professor_Ashu/83")
+                ]
+            ]
+        ),
+    )
